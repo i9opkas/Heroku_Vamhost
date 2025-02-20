d48d7966aaf1bd452a1ecb6a56f2d27d73e773b6
Vamics_git <ooopkas@gmail.com>
2025-02-20 21:24:29 +0200

    Update update_notifier.py

--- hikka/modules/update_notifier.py
+++ hikka/modules/update_notifier.py
@@ -97,7 +97,7 @@ class UpdateNotifier(loader.Module):
                 "https://t.me/hikari_assets/71",
                 caption=self.strings("update_required").format(
                     utils.get_git_hash()[:6],
-                    '<a href="https://github.com/coddrago/Heroku/compare/{}...{}">{}</a>'.format(
+                    '<a href="https://github.com/i9opkas/Heroku_Vamhost/compare/{}...{}">{}</a>'.format(
                         utils.get_git_hash()[:12],
                         self.get_latest()[:12],
                         self.get_latest()[:6],
