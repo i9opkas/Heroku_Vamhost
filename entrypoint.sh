03bc0798159fd3bc546e7d906d4515491c6728ab
Vamics_git <ooopkas@gmail.com>
2025-02-21 23:12:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ echo "Налаштовуємо iptables..."
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 8080 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 22 -j DROP
 iptables -A OUTPUT -p tcp -j DROP
