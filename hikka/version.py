078ead114c7de9677ef3ad418518384da142c862
Vamics_git <ooopkas@gmail.com>
2025-02-21 14:29:32 +0200

    Update version.py

--- hikka/version.py
+++ hikka/version.py
@@ -5,15 +5,20 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
-__version__ = (1, 6, 8.1)
+__version__ = (1, 6, 8, 1)  # Виправлена крапка на кому
 
 import os
 
-import git
-
 try:
-    branch = git.Repo(
-        path=os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    ).active_branch.name
-except Exception:
+    import git
+
+    repo_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    repo = git.Repo(path=repo_path, search_parent_directories=True)
+
+    if repo.bare:
+        branch = "unknown"
+    else:
+        branch = repo.active_branch.name
+
+except (ImportError, AttributeError, ValueError):
     branch = "master"
