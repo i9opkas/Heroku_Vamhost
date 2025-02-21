9069d8878c50a7e1781975a6505ffe6f2871f564
Vamics_git <ooopkas@gmail.com>
2025-02-21 18:55:36 +0200

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -8,18 +8,14 @@ import asyncio
 import contextlib
 import logging
 import os
-import subprocess
-import sys
 import time
 import typing
 import datetime
-
 import git
 from git import GitCommandError, Repo
-from hikkatl.extensions.html import CUSTOM_EMOJIS
-from hikkatl.tl.functions.messages import GetDialogFiltersRequest, UpdateDialogFilterRequest
-from hikkatl.tl.types import DialogFilter, Message
 
+from hikkatl.extensions.html import CUSTOM_EMOJIS
+from hikkatl.tl.types import Message
 from .. import loader, main, utils, version
 from .._internal import restart
 from ..inline.types import InlineCall
@@ -28,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 @loader.tds
 class UpdaterMod(loader.Module):
-    """Updates itself"""
+    """Self-update module for Hikka"""
 
     strings = {"name": "Updater"}
 
@@ -41,25 +37,26 @@ class UpdaterMod(loader.Module):
                 validator=loader.validators.Link(),
             )
         )
-        asyncio.create_task(self.schedule_restart())  # Запуск фоновой задачи
+        asyncio.create_task(self.schedule_restart())
 
     async def schedule_restart(self):
         while True:
-            now = datetime.datetime.utcnow() + datetime.timedelta(hours=3)  
+            now = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
             next_run = now.replace(hour=0, minute=0, second=0, microsecond=0)
             if now >= next_run:
-                next_run += datetime.timedelta(days=1) 
+                next_run += datetime.timedelta(days=1)
 
             wait_time = (next_run - now).total_seconds()
             await asyncio.sleep(wait_time)
 
-            logger.info("Выполняю перезапуск...")
+            logger.info("Performing scheduled restart...")
             await self.restart_common(None)
 
     @loader.command()
     async def restart(self, message: Message):
         args = utils.get_args_raw(message)
         secure_boot = any(trigger in args for trigger in {"--secure-boot", "-sb"})
+
         try:
             if (
                 "-f" in args
@@ -95,7 +92,6 @@ class UpdaterMod(loader.Module):
             self._db.set(loader.__name__, "secure_boot", True)
 
         self.set("restart_ts", time.time())
-
         await self._db.remote_force_save()
 
         if "LAVHOST" in os.environ:
@@ -108,12 +104,14 @@ class UpdaterMod(loader.Module):
         handler = logging.getLogger().handlers[0]
         handler.setLevel(logging.CRITICAL)
 
-        for client in self.allclients:
-            if not isinstance(msg_obj, InlineCall) or client is not msg_obj._client:
-                await client.disconnect()
+        if isinstance(msg_obj, Message):
+            for client in self.allclients:
+                if client is not msg_obj.client:
+                    await client.disconnect()
 
-        if isinstance(msg_obj, (InlineCall, Message)):
-            await msg_obj._client.disconnect()
+            await msg_obj.client.disconnect()
+        else:
+            logger.warning("msg_obj is not a Message instance. Skipping client disconnect.")
 
         restart()
 
@@ -195,7 +193,7 @@ class UpdaterMod(loader.Module):
                 await self.inline_update(msg_obj, True)
                 return
 
-            logger.critical("Got update loop. Update manually via .terminal")
+            logger.critical("Update loop detected. Manual update required via .terminal")
 
     async def client_ready(self):
         if self.get("selfupdatemsg") is not None:
@@ -207,15 +205,10 @@ class UpdaterMod(loader.Module):
         if self.get("do_not_create", False):
             return
 
-        try:
-            await self._add_folder()
-        except Exception:
-            logger.exception("Failed to add folder!")
-
         self.set("do_not_create", True)
 
     async def update_complete(self):
-        logger.debug("Self update successful! Edit message")
+        logger.debug("Self update successful! Editing message")
         start = self.get("restart_ts")
         try:
             took = round(time.time() - start)
@@ -234,4 +227,4 @@ class UpdaterMod(loader.Module):
         await self.inline.bot.edit_message_text(
             inline_message_id=ms,
             text=self.inline.sanitise_text(msg),
-            )
+)
