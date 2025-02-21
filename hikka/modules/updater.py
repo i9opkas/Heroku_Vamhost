5041fe9b3d337d8cfa2f77ede03db08ab3805af8
Vamics_git <ooopkas@gmail.com>
2025-02-21 10:13:15 +0200

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -36,7 +36,7 @@ class UpdaterMod(loader.Module):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "GIT_ORIGIN_URL",
-                "https://github.com/i9opkas/Heroku_Vamhost",
+                "https://github.com/coddrago/Heroku",
                 lambda: self.strings("origin_cfg_doc"),
                 validator=loader.validators.Link(),
             )
