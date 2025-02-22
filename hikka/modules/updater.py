2852f0c74458246e5093c1e0be1ed2ea243a445d
Vamics_git <ooopkas@gmail.com>
2025-02-22 18:28:07 +0200

    Update updater.py (debug)

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -38,7 +38,7 @@ class UpdaterMod(loader.Module):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "GIT_ORIGIN_URL",
-                "https://github.com/coddrago/Heroku",
+                "https://github.com/i9opkas/Heroku_Vamhost",
                 lambda: self.strings("origin_cfg_doc"),
                 validator=loader.validators.Link(),
             )
@@ -166,7 +166,7 @@ class UpdaterMod(loader.Module):
         try:
             subprocess.run(
                 [
-                    sys.executable,
+                    "python3",
                     "-m",
                     "pip",
                     "install",
