c90dd14d2afa588b4f7a4fdae528ce0df5ac210f
Vamics_git <ooopkas@gmail.com>
2025-02-22 13:41:50 +0200

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -45,125 +45,36 @@ class UpdaterMod(loader.Module):
             )
         )
 
-    @loader.command()
-    async def restart(self, message: Message):
-        args = utils.get_args_raw(message)
-        secure_boot = any(trigger in args for trigger in {"--secure-boot", "-sb"})
-        try:
-            if (
-                "-f" in args
-                or not self.inline.init_complete
-                or not await self.inline.form(
-                    message=message,
-                    text=self.strings(
-                        "secure_boot_confirm" if secure_boot else "restart_confirm"
-                    ),
-                    reply_markup=[
-                        {
-                            "text": self.strings("btn_restart"),
-                            "callback": self.inline_restart,
-                            "args": (secure_boot,),
-                        },
-                        {"text": self.strings("cancel"), "action": "close"},
-                    ],
-                )
-            ):
-                raise
-        except Exception:
-            await self.restart_common(message, secure_boot)
-
-    async def inline_restart(self, call: InlineCall, secure_boot: bool = False):
-        await self.restart_common(call, secure_boot=secure_boot)
-
-    async def process_restart_message(self, msg_obj: typing.Union[InlineCall, Message]):
-        self.set(
-            "selfupdatemsg",
-            (
-                msg_obj.inline_message_id
-                if hasattr(msg_obj, "inline_message_id")
-                else f"{utils.get_chat_id(msg_obj)}:{msg_obj.id}"
-            ),
-        )
-
-    async def restart_common(
-        self,
-        msg_obj: typing.Union[InlineCall, Message],
-        secure_boot: bool = False,
-    ):
-        if (
-            hasattr(msg_obj, "form")
-            and isinstance(msg_obj.form, dict)
-            and "uid" in msg_obj.form
-            and msg_obj.form["uid"] in self.inline._units
-            and "message" in self.inline._units[msg_obj.form["uid"]]
-        ):
-            message = self.inline._units[msg_obj.form["uid"]]["message"]
-        else:
-            message = msg_obj
-
-        if secure_boot:
-            self._db.set(loader.__name__, "secure_boot", True)
-
-        msg_obj = await utils.answer(
-            msg_obj,
-            self.strings("restarting_caption").format(
-                utils.get_platform_emoji()
-                if self._client.hikka_me.premium
-                and CUSTOM_EMOJIS
-                and isinstance(msg_obj, Message)
-                else "Heroku"
-            ),
-        )
-
-        await self.process_restart_message(msg_obj)
-
-        self.set("restart_ts", time.time())
-
-        await self._db.remote_force_save()
-
-        if "LAVHOST" in os.environ:
-            os.system("lavhost restart")
-            return
-
-        with contextlib.suppress(Exception):
-            await main.hikka.web.stop()
-
-        handler = logging.getLogger().handlers[0]
-        handler.setLevel(logging.CRITICAL)
-
-        for client in self.allclients:
-            # Terminate main loop of all running clients
-            # Won't work if not all clients are ready
-            if client is not message.client:
-                await client.disconnect()
-
-        await message.client.disconnect()
-        restart()
-
     async def download_common(self):
         try:
-            repo = Repo(os.path.dirname(utils.get_base_dir()))
-            origin = repo.remote("origin")
-            r = origin.pull()
+            repo_path = os.path.dirname(utils.get_base_dir())
+            repo = Repo(repo_path)
+
+            # Выполняем команду обновления через git checkout + pull
+            subprocess.run(
+                ["git", "checkout", "origin/master"],
+                cwd=repo_path,
+                check=True,
+            )
+            subprocess.run(
+                ["git", "pull", "origin", "master"],
+                cwd=repo_path,
+                check=True,
+            )
+
+            # Проверяем изменения в requirements.txt
             new_commit = repo.head.commit
-            for info in r:
-                if info.old_commit:
-                    for d in new_commit.diff(info.old_commit):
-                        if d.b_path == "requirements.txt":
-                            return True
+            for d in new_commit.diff("HEAD~1"):
+                if d.b_path == "requirements.txt":
+                    return True
             return False
-        except git.exc.InvalidGitRepositoryError:
-            repo = Repo.init(os.path.dirname(utils.get_base_dir()))
-            origin = repo.create_remote("origin", self.config["GIT_ORIGIN_URL"])
-            origin.fetch()
-            repo.create_head("master", origin.refs.master)
-            repo.heads.master.set_tracking_branch(origin.refs.master)
-            repo.heads.master.checkout(True)
+        except (git.exc.InvalidGitRepositoryError, subprocess.CalledProcessError) as e:
+            logger.error(f"Git update error: {e}")
             return False
 
     @staticmethod
     def req_common():
-        # Now we have downloaded new code, install requirements
+        """Установка новых зависимостей, если они появились"""
         logger.debug("Installing new requirements...")
         try:
             subprocess.run(
@@ -184,49 +95,12 @@ class UpdaterMod(loader.Module):
         except subprocess.CalledProcessError:
             logger.exception("Req install failed")
 
-    @loader.command()
-    async def update(self, message: Message):
-        try:
-            args = utils.get_args_raw(message)
-            current = utils.get_git_hash()
-            upcoming = next(
-                git.Repo().iter_commits(f"origin/{version.branch}", max_count=1)
-            ).hexsha
-            if (
-                "-f" in args
-                or not self.inline.init_complete
-                or not await self.inline.form(
-                    message=message,
-                    text=(
-                        self.strings("update_confirm").format(
-                            current, current[:8], upcoming, upcoming[:8]
-                        )
-                        if upcoming != current
-                        else self.strings("no_update")
-                    ),
-                    reply_markup=[
-                        {
-                            "text": self.strings("btn_update"),
-                            "callback": self.inline_update,
-                        },
-                        {"text": self.strings("cancel"), "action": "close"},
-                    ],
-                )
-            ):
-                raise
-        except Exception:
-            await self.inline_update(message)
-
-    async def close(self, msg_obj: typing.Union[InlineCall, Message], hard: bool = False,):
-        
-        await call.delete()
-
     async def inline_update(
         self,
         msg_obj: typing.Union[InlineCall, Message],
         hard: bool = False,
     ):
-        # We don't really care about asyncio at this point, as we are shutting down
+        """Основной процесс обновления"""
         if hard:
             os.system(f"cd {utils.get_base_dir()} && cd .. && git reset --hard HEAD")
 
@@ -234,16 +108,7 @@ class UpdaterMod(loader.Module):
             if "LAVHOST" in os.environ:
                 msg_obj = await utils.answer(
                     msg_obj,
-                    self.strings("lavhost_update").format(
-                        "</b><emoji document_id=5192756799647785066>✌️</emoji><emoji"
-                        " document_id=5193117564015747203>✌️</emoji><emoji"
-                        " document_id=5195050806105087456>✌️</emoji><emoji"
-                        " document_id=5195457642587233944>✌️</emoji><b>"
-                        if self._client.hikka_me.premium
-                        and CUSTOM_EMOJIS
-                        and isinstance(msg_obj, Message)
-                        else "lavHost"
-                    ),
+                    self.strings("lavhost_update").format("lavHost"),
                 )
                 await self.process_restart_message(msg_obj)
                 os.system("lavhost update")
@@ -267,170 +132,3 @@ class UpdaterMod(loader.Module):
                 return
 
             logger.critical("Got update loop. Update manually via .terminal")
-
-    @loader.command()
-    async def source(self, message: Message):
-        await utils.answer(
-            message,
-            self.strings("source").format(self.config["GIT_ORIGIN_URL"]),
-        )
-
-    async def client_ready(self):
-        if self.get("selfupdatemsg") is not None:
-            try:
-                await self.update_complete()
-            except Exception:
-                logger.exception("Failed to complete update!")
-
-        if self.get("do_not_create", False):
-            return
-
-        try:
-            await self._add_folder()
-        except Exception:
-            logger.exception("Failed to add folder!")
-
-        self.set("do_not_create", True)
-
-    async def _add_folder(self):
-        folders = await self._client(GetDialogFiltersRequest())
-
-        if any(getattr(folder, "title", None) == "hikka" for folder in folders):
-            return
-
-        try:
-            folder_id = (
-                max(
-                    (folder for folder in folders if hasattr(folder, "id")),
-                    key=lambda x: x.id,
-                ).id
-                + 1
-            )
-        except ValueError:
-            folder_id = 2
-
-        try:
-            await self._client(
-                UpdateDialogFilterRequest(
-                    folder_id,
-                    DialogFilter(
-                        folder_id,
-                        title="hikka",
-                        pinned_peers=(
-                            [
-                                await self._client.get_input_entity(
-                                    self._client.loader.inline.bot_id
-                                )
-                            ]
-                            if self._client.loader.inline.init_complete
-                            else []
-                        ),
-                        include_peers=[
-                            await self._client.get_input_entity(dialog.entity)
-                            async for dialog in self._client.iter_dialogs(
-                                None,
-                                ignore_migrated=True,
-                            )
-                            if dialog.name
-                            in {
-                                "heroku-logs",
-                                "heroku-onload",
-                                "heroku-assets",
-                                "heroku-backups",
-                                "heroku-acc-switcher",
-                                "silent-tags",
-                            }
-                            and dialog.is_channel
-                            and (
-                                dialog.entity.participants_count == 1
-                                or dialog.entity.participants_count == 2
-                                and dialog.name in {"hikka-logs", "silent-tags"}
-                            )
-                            or (
-                                self._client.loader.inline.init_complete
-                                and dialog.entity.id
-                                == self._client.loader.inline.bot_id
-                            )
-                            or dialog.entity.id
-                            in [
-                                1554874075,
-                                1697279580,
-                                1679998924,
-                                2410964167,
-                            ]  # official heroku chats
-                        ],
-                        emoticon="🐱",
-                        exclude_peers=[],
-                        contacts=False,
-                        non_contacts=False,
-                        groups=False,
-                        broadcasts=False,
-                        bots=False,
-                        exclude_muted=False,
-                        exclude_read=False,
-                        exclude_archived=False,
-                    ),
-                )
-            )
-        except Exception:
-            logger.critical(
-                "Can't create Heroku folder. Possible reasons are:\n"
-                "- User reached the limit of folders in Telegram\n"
-                "- User got floodwait\n"
-                "Ignoring error and adding folder addition to ignore list"
-            )
-
-    async def update_complete(self):
-        logger.debug("Self update successful! Edit message")
-        start = self.get("restart_ts")
-        try:
-            took = round(time.time() - start)
-        except Exception:
-            took = "n/a"
-
-        msg = self.strings("success").format(utils.ascii_face(), took)
-        ms = self.get("selfupdatemsg")
-
-        if ":" in str(ms):
-            chat_id, message_id = ms.split(":")
-            chat_id, message_id = int(chat_id), int(message_id)
-            await self._client.edit_message(chat_id, message_id, msg)
-            return
-
-        await self.inline.bot.edit_message_text(
-            inline_message_id=ms,
-            text=self.inline.sanitise_text(msg),
-        )
-
-    async def full_restart_complete(self, secure_boot: bool = False):
-        start = self.get("restart_ts")
-
-        try:
-            took = round(time.time() - start)
-        except Exception:
-            took = "n/a"
-
-        self.set("restart_ts", None)
-
-        ms = self.get("selfupdatemsg")
-        msg = self.strings(
-            "secure_boot_complete" if secure_boot else "full_success"
-        ).format(utils.ascii_face(), took)
-
-        if ms is None:
-            return
-
-        self.set("selfupdatemsg", None)
-
-        if ":" in str(ms):
-            chat_id, message_id = ms.split(":")
-            chat_id, message_id = int(chat_id), int(message_id)
-            await self._client.edit_message(chat_id, message_id, msg)
-            await asyncio.sleep(60)
-            await self._client.delete_messages(chat_id, message_id)
-            return
-
-        await self.inline.bot.edit_message_text(
-            inline_message_id=ms,
-            text=self.inline.sanitise_text(msg),
-            )
