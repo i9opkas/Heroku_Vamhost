9b6e4cc884b4315fc3210d7297cbaee76b8a9ac6
Vamics_git <ooopkas@gmail.com>
2025-02-22 08:40:59 +0200

    Update version.py

--- hikka/version.py
+++ hikka/version.py
@@ -1,15 +1,14 @@
 """Represents current userbot version"""
 # ©️ Dan Gazizullin, 2021-2023
-# This file is a part of Hikka Userbot (test3)
+# This file is a part of Hikka Userbot
 # 🌐 https://github.com/hikariatama/Hikka
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
-__version__ = (1, 6, 8) 
+__version__ = (1, 6, 8)
 
 import os
 import git
-
 try:
     branch = git.Repo(
         path=os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
