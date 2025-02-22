e1d40b6a0b5d3e6f467c8049995e701a6c7fd47d
Vamics_git <ooopkas@gmail.com>
2025-02-22 08:31:42 +0200

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -8,23 +8,30 @@ import asyncio
 import contextlib
 import logging
 import os
+import subprocess
+import sys
 import time
 import typing
-import datetime
+
 import git
 from git import GitCommandError, Repo
-
 from hikkatl.extensions.html import CUSTOM_EMOJIS
-from hikkatl.tl.types import Message
+from hikkatl.tl.functions.messages import (
+    GetDialogFiltersRequest,
+    UpdateDialogFilterRequest,
+)
+from hikkatl.tl.types import DialogFilter, Message
+
 from .. import loader, main, utils, version
 from .._internal import restart
 from ..inline.types import InlineCall
 
 logger = logging.getLogger(__name__)
 
+
 @loader.tds
 class UpdaterMod(loader.Module):
-    """Self-update module for Hikka"""
+    """Updates itself"""
 
     strings = {"name": "Updater"}
 
@@ -37,26 +44,11 @@ class UpdaterMod(loader.Module):
                 validator=loader.validators.Link(),
             )
         )
-        asyncio.create_task(self.schedule_restart())
-
-    async def schedule_restart(self):
-        while True:
-            now = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
-            next_run = now.replace(hour=0, minute=0, second=0, microsecond=0)
-            if now >= next_run:
-                next_run += datetime.timedelta(days=1)
-
-            wait_time = (next_run - now).total_seconds()
-            await asyncio.sleep(wait_time)
-
-            logger.info("Performing scheduled restart...")
-            await self.restart_common(None)
 
     @loader.command()
     async def restart(self, message: Message):
         args = utils.get_args_raw(message)
         secure_boot = any(trigger in args for trigger in {"--secure-boot", "-sb"})
-
         try:
             if (
                 "-f" in args
@@ -83,15 +75,50 @@ class UpdaterMod(loader.Module):
     async def inline_restart(self, call: InlineCall, secure_boot: bool = False):
         await self.restart_common(call, secure_boot=secure_boot)
 
+    async def process_restart_message(self, msg_obj: typing.Union[InlineCall, Message]):
+        self.set(
+            "selfupdatemsg",
+            (
+                msg_obj.inline_message_id
+                if hasattr(msg_obj, "inline_message_id")
+                else f"{utils.get_chat_id(msg_obj)}:{msg_obj.id}"
+            ),
+        )
+
     async def restart_common(
         self,
-        msg_obj: typing.Union[InlineCall, Message, None],
+        msg_obj: typing.Union[InlineCall, Message],
         secure_boot: bool = False,
     ):
+        if (
+            hasattr(msg_obj, "form")
+            and isinstance(msg_obj.form, dict)
+            and "uid" in msg_obj.form
+            and msg_obj.form["uid"] in self.inline._units
+            and "message" in self.inline._units[msg_obj.form["uid"]]
+        ):
+            message = self.inline._units[msg_obj.form["uid"]]["message"]
+        else:
+            message = msg_obj
+
         if secure_boot:
             self._db.set(loader.__name__, "secure_boot", True)
 
+        msg_obj = await utils.answer(
+            msg_obj,
+            self.strings("restarting_caption").format(
+                utils.get_platform_emoji()
+                if self._client.hikka_me.premium
+                and CUSTOM_EMOJIS
+                and isinstance(msg_obj, Message)
+                else "Heroku"
+            ),
+        )
+
+        await self.process_restart_message(msg_obj)
+
         self.set("restart_ts", time.time())
+
         await self._db.remote_force_save()
 
         if "LAVHOST" in os.environ:
@@ -104,15 +131,13 @@ class UpdaterMod(loader.Module):
         handler = logging.getLogger().handlers[0]
         handler.setLevel(logging.CRITICAL)
 
-        if isinstance(msg_obj, Message):
-            for client in self.allclients:
-                if client is not msg_obj.client:
-                    await client.disconnect()
+        for client in self.allclients:
+            # Terminate main loop of all running clients
+            # Won't work if not all clients are ready
+            if client is not message.client:
+                await client.disconnect()
 
-            await msg_obj.client.disconnect()
-        else:
-            logger.warning("msg_obj is not a Message instance. Skipping client disconnect.")
-
+        await message.client.disconnect()
         restart()
 
     async def download_common(self):
@@ -135,6 +160,29 @@ class UpdaterMod(loader.Module):
             repo.heads.master.set_tracking_branch(origin.refs.master)
             repo.heads.master.checkout(True)
             return False
+
+    @staticmethod
+    def req_common():
+        # Now we have downloaded new code, install requirements
+        logger.debug("Installing new requirements...")
+        try:
+            subprocess.run(
+                [
+                    sys.executable,
+                    "-m",
+                    "pip",
+                    "install",
+                    "-r",
+                    os.path.join(
+                        os.path.dirname(utils.get_base_dir()),
+                        "requirements.txt",
+                    ),
+                    "--user",
+                ],
+                check=True,
+            )
+        except subprocess.CalledProcessError:
+            logger.exception("Req install failed")
 
     @loader.command()
     async def update(self, message: Message):
@@ -169,7 +217,16 @@ class UpdaterMod(loader.Module):
         except Exception:
             await self.inline_update(message)
 
-    async def inline_update(self, msg_obj: typing.Union[InlineCall, Message], hard: bool = False):
+    async def close(self, msg_obj: typing.Union[InlineCall, Message], hard: bool = False,):
+        
+        await call.delete()
+
+    async def inline_update(
+        self,
+        msg_obj: typing.Union[InlineCall, Message],
+        hard: bool = False,
+    ):
+        # We don't really care about asyncio at this point, as we are shutting down
         if hard:
             os.system(f"cd {utils.get_base_dir()} && cd .. && git reset --hard HEAD")
 
@@ -177,12 +234,28 @@ class UpdaterMod(loader.Module):
             if "LAVHOST" in os.environ:
                 msg_obj = await utils.answer(
                     msg_obj,
-                    self.strings("lavhost_update"),
+                    self.strings("lavhost_update").format(
+                        "</b><emoji document_id=5192756799647785066>✌️</emoji><emoji"
+                        " document_id=5193117564015747203>✌️</emoji><emoji"
+                        " document_id=5195050806105087456>✌️</emoji><emoji"
+                        " document_id=5195457642587233944>✌️</emoji><b>"
+                        if self._client.hikka_me.premium
+                        and CUSTOM_EMOJIS
+                        and isinstance(msg_obj, Message)
+                        else "lavHost"
+                    ),
                 )
+                await self.process_restart_message(msg_obj)
                 os.system("lavhost update")
                 return
 
+            with contextlib.suppress(Exception):
+                msg_obj = await utils.answer(msg_obj, self.strings("downloading"))
+
             req_update = await self.download_common()
+
+            with contextlib.suppress(Exception):
+                msg_obj = await utils.answer(msg_obj, self.strings("installing"))
 
             if req_update:
                 self.req_common()
@@ -193,7 +266,14 @@ class UpdaterMod(loader.Module):
                 await self.inline_update(msg_obj, True)
                 return
 
-            logger.critical("Update loop detected. Manual update required via .terminal")
+            logger.critical("Got update loop. Update manually via .terminal")
+
+    @loader.command()
+    async def source(self, message: Message):
+        await utils.answer(
+            message,
+            self.strings("source").format(self.config["GIT_ORIGIN_URL"]),
+        )
 
     async def client_ready(self):
         if self.get("selfupdatemsg") is not None:
@@ -205,10 +285,103 @@ class UpdaterMod(loader.Module):
         if self.get("do_not_create", False):
             return
 
+        try:
+            await self._add_folder()
+        except Exception:
+            logger.exception("Failed to add folder!")
+
         self.set("do_not_create", True)
 
+    async def _add_folder(self):
+        folders = await self._client(GetDialogFiltersRequest())
+
+        if any(getattr(folder, "title", None) == "hikka" for folder in folders):
+            return
+
+        try:
+            folder_id = (
+                max(
+                    (folder for folder in folders if hasattr(folder, "id")),
+                    key=lambda x: x.id,
+                ).id
+                + 1
+            )
+        except ValueError:
+            folder_id = 2
+
+        try:
+            await self._client(
+                UpdateDialogFilterRequest(
+                    folder_id,
+                    DialogFilter(
+                        folder_id,
+                        title="hikka",
+                        pinned_peers=(
+                            [
+                                await self._client.get_input_entity(
+                                    self._client.loader.inline.bot_id
+                                )
+                            ]
+                            if self._client.loader.inline.init_complete
+                            else []
+                        ),
+                        include_peers=[
+                            await self._client.get_input_entity(dialog.entity)
+                            async for dialog in self._client.iter_dialogs(
+                                None,
+                                ignore_migrated=True,
+                            )
+                            if dialog.name
+                            in {
+                                "heroku-logs",
+                                "heroku-onload",
+                                "heroku-assets",
+                                "heroku-backups",
+                                "heroku-acc-switcher",
+                                "silent-tags",
+                            }
+                            and dialog.is_channel
+                            and (
+                                dialog.entity.participants_count == 1
+                                or dialog.entity.participants_count == 2
+                                and dialog.name in {"hikka-logs", "silent-tags"}
+                            )
+                            or (
+                                self._client.loader.inline.init_complete
+                                and dialog.entity.id
+                                == self._client.loader.inline.bot_id
+                            )
+                            or dialog.entity.id
+                            in [
+                                1554874075,
+                                1697279580,
+                                1679998924,
+                                2410964167,
+                            ]  # official heroku chats
+                        ],
+                        emoticon="🐱",
+                        exclude_peers=[],
+                        contacts=False,
+                        non_contacts=False,
+                        groups=False,
+                        broadcasts=False,
+                        bots=False,
+                        exclude_muted=False,
+                        exclude_read=False,
+                        exclude_archived=False,
+                    ),
+                )
+            )
+        except Exception:
+            logger.critical(
+                "Can't create Heroku folder. Possible reasons are:\n"
+                "- User reached the limit of folders in Telegram\n"
+                "- User got floodwait\n"
+                "Ignoring error and adding folder addition to ignore list"
+            )
+
     async def update_complete(self):
-        logger.debug("Self update successful! Editing message")
+        logger.debug("Self update successful! Edit message")
         start = self.get("restart_ts")
         try:
             took = round(time.time() - start)
@@ -227,4 +400,37 @@ class UpdaterMod(loader.Module):
         await self.inline.bot.edit_message_text(
             inline_message_id=ms,
             text=self.inline.sanitise_text(msg),
-)
+        )
+
+    async def full_restart_complete(self, secure_boot: bool = False):
+        start = self.get("restart_ts")
+
+        try:
+            took = round(time.time() - start)
+        except Exception:
+            took = "n/a"
+
+        self.set("restart_ts", None)
+
+        ms = self.get("selfupdatemsg")
+        msg = self.strings(
+            "secure_boot_complete" if secure_boot else "full_success"
+        ).format(utils.ascii_face(), took)
+
+        if ms is None:
+            return
+
+        self.set("selfupdatemsg", None)
+
+        if ":" in str(ms):
+            chat_id, message_id = ms.split(":")
+            chat_id, message_id = int(chat_id), int(message_id)
+            await self._client.edit_message(chat_id, message_id, msg)
+            await asyncio.sleep(60)
+            await self._client.delete_messages(chat_id, message_id)
+            return
+
+        await self.inline.bot.edit_message_text(
+            inline_message_id=ms,
+            text=self.inline.sanitise_text(msg),
+            )
