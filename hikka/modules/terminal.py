69de7925b6775f2f2980b892afedbc0ee1cbcb1d
Vamics_git <ooopkas@gmail.com>
2025-02-23 14:16:12 +0200

    Update terminal.py
    Добавлена логика блокировки команд

--- hikka/modules/terminal.py
+++ hikka/modules/terminal.py
@@ -298,7 +298,7 @@ class TerminalMod(loader.Module):
     """Runs commands"""
 
     strings = {"name": "Terminal"}
-
+    BLOCKED_COMMANDS = ["kill", "socat", "exit"]
     def __init__(self):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
@@ -330,13 +330,17 @@ class TerminalMod(loader.Module):
                 True,
             ),
         )
-
+        
     async def run_command(
         self,
         message: hikkatl.tl.types.Message,
         cmd: str,
         editor: typing.Optional[MessageEditor] = None,
     ):
+        if any(blocked_cmd in cmd for blocked_cmd in self.BLOCKED_COMMANDS):
+            await utils.answer(message, "it's command blocked.")
+            return
+
         if len(cmd.split(" ")) > 1 and cmd.split(" ")[0] == "sudo":
             needsswitch = True
 
@@ -379,6 +383,9 @@ class TerminalMod(loader.Module):
                 self.config["FLOOD_WAIT_PROTECT"],
             ),
         )
+
+        await editor.cmd_ended(await sproc.wait())
+        del self.activecmds[hash_msg(message)]
 
         await editor.cmd_ended(await sproc.wait())
         del self.activecmds[hash_msg(message)]
