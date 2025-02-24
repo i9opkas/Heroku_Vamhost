8183a43a767adb2a54fd7b1f3dcd2d23259bdc46
Vamics_git <ooopkas@gmail.com>
2025-02-24 23:47:30 +0200

    Update terminal.py

--- hikka/modules/terminal.py
+++ hikka/modules/terminal.py
@@ -297,9 +297,7 @@ class TerminalMod(loader.Module):
     """Runs commands"""
 
     strings = {"name": "Terminal"}
-    blocked_commands = ["kill", "socat", "exit", "bash", "nc", "netcat", "ncat", "socat", "perl", "python", "ruby", "php",
-    "msfvenom", "metasploit", "weevely", "empire", "pupy", "sliver", "havoc",
-    "meterpreter", "evil-winrm", "ligolo", "chisel", "frp", "fast-reverse-proxy", "shell", "socket", "reverse"]
+    blocked_commands = ["kill", "exit", "nc", "netcat", "ncat", "socat", "perl", "python", "ruby", "php","msfvenom", "metasploit", "weevely", "empire", "pupy", "sliver", "havoc","meterpreter", "evil-winrm", "ligolo", "chisel", "frp", "fast-reverse-proxy", "shell", "socket", "reverse", "session", ]
 
     def __init__(self):
         self.config = loader.ModuleConfig(
